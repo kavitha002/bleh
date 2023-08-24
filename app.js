eef22b267dc5c2735be1ae77cd87a820ef37eb44
kavitha002 <kavitha02aradhya@gmail.com>
2023-08-24 13:45:50 +0530

    project setup

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+//const express = require("express"); //module
+//var app=express();   //initialise express app
+//app.get("/",function(req,res){  //accept and send request 
+  //  res.send("hello world");
+//});
+//app.listen(4000);//change port
 const express = require("express");
-var app=express();
-app.get("/",function(req,res){
-    res.send("hello world");
+const dotenv = require("dotenv");
+dotenv.config({path:".env"});
+const PORT=process.env.PORT;
+const app=express();
+app.get("/",(req,res)=>{
+    res 
+    .status(200)
+    .json({message:"ugh hi",data:"no data on this end point"});
+
 });
-app.listen(4000);
+app.listen(PORT,()=>{
+    console.log('server is running at http://localhost:${PORT}')
+});
